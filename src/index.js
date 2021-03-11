3060456ab630f122284174c1d9e34c26b84e2476
David Bassey <davidosky1@gmail.com>
2021-03-11 11:30:24 +0100

    Add file loader and more images

--- src/index.js
+++ src/index.js
@@ -1,1 +1,25 @@
-console.log('I am David Bassey a Software Developer and i am back');
+import './style.css';
+import Amala from '../assets/images/background1.jpg';
+
+function component() {
+  const element = document.createElement('div');
+
+  element.innerHTML = `<div class="container hello d-flex">
+  <h3>Breakfast</h3>
+ <div class="d-block">
+ <span>Omelette</span>
+         <span>Coffee</span>
+         <span>Tea</span>
+         <span>Bread</span>
+ 
+ </div>
+
+ </div>`;
+ const myAmala = new Image();
+ myAmala.src = Amala;
+ element.appendChild(myAmala);
+ 
+  return element;
+}
+
+document.body.appendChild(component());
